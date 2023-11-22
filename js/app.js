c5cf379dfd47139344c0814325aa8e8caff7f447
nicoIosa <nicolasiosa@hotmail.com>
2023-11-22 11:27:52 -0300

    ejercicio termiando

--- js/app.js
+++ js/app.js
@@ -1,15 +1,44 @@
+const btn = document.getElementsByClassName("btn");
+const paragraphs = document.getElementsByTagName("p");
+const list = document.querySelector("ul");
+const formNumber = document.querySelector("#formNumber");
+let randomNumber;
+
 function startGame() {
-  console.log("Estoy anashe");
-  const btn = document.getElementsByClassName("btn");
-  const input = document.getElementById("input");
-  const paragraph = document.getElementsByTagName("p");
-  const randomNumber = Math.floor(Math.random() * 99);
-  console.log(btn);
-  console.log(input);
-  console.log(paragraph);
+  randomNumber = Math.floor(Math.random() * 99);
   console.log(randomNumber);
   btn[0].className += " d-none";
-  input.className = "row justify-content-center";
-  paragraph[0].innerHTML = "Ya elegi el numero, puedes adivinar";
-  paragraph[0].className = "lead mb-5";
+  formNumber.className = "row justify-content-center";
+  paragraphs[0].className = "lead mb-5";
 }
+
+function guess(e) {
+  e.preventDefault();
+  const inputNumber = document.getElementById("inputNumber").value;
+  if (inputNumber >= 1 && inputNumber <= 100) {
+    if (inputNumber == randomNumber) {
+      window.alert("Adivinaste el numero");
+      btn[0].className = "btn btn-primary mb-5 btn-lg";
+      formNumber.className += " d-none";
+      paragraphs[0].className += " d-none";
+      paragraphs[1].className = "lead mb-5";
+      list.className = "list-group list-group-flush";
+      const newEntry = document.createElement("li");
+      newEntry.innerHTML = inputNumber;
+      newEntry.className = "list-group-item";
+      list.appendChild(newEntry);
+    } else {
+      window.alert("No adivinaste el numero");
+      if (inputNumber < randomNumber) {
+        alert("El numero que ingresaste es menor al numero magico");
+      } else {
+        alert("El numero que ingresaste es mayor al numero magico");
+      }
+    }
+  } else {
+    alert("No ingresaste un numero valido")
+  }
+  formNumber.reset();
+}
+
+formNumber.addEventListener("submit", guess);
